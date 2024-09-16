8d85ac055e6f2b18a8734d4e8c3becef2e731e2c
NewAsala <asalagithub@gmail.com>
2024-09-16 15:53:16 +0300

    therapies migrate

--- database/migrations/2024_09_06_120844_create_therapies_table.php
+++ database/migrations/2024_09_06_120844_create_therapies_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('therapies', function (Blueprint $table) {
             $table->id();
             $table->text('name');
-            $table->string('file');
+            $table->text('file');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
